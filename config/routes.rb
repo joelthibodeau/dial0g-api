0235580b8b0db9196e8c67d9b414f48fd72a0ad9
joelthibodeau <joel.thibodeau@gmail.com>
2018-11-26 16:43:55 -0500

    Fix path names in auth curl scripts.
    Additional:
    - remove extra '/' in file path name.
    - update routes file with dialog info.

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,11 @@ Rails.application.routes.draw do
   post '/sign-in' => 'users#signin'
   delete '/sign-out' => 'users#signout'
   patch '/change-password' => 'users#changepw'
+
+  # Dialog routes
+  get '/dialogs' => 'dialogs#index'
+  get '/dialogs/:id' => 'dialogs#show'
+  post '/dialogs' => 'dialogs#create'
+  patch '/dialogs/:id' => 'dialogs#update'
+  delete '/dialogs/:id' => 'dialogs#destroy'
 end
