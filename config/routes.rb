2fefc1aee85c4140a42a7667acd137ab41f09521
joelthibodeau <joel.thibodeau@gmail.com>
2018-11-28 13:40:42 -0500

    Fix routes file from '%i[new end]' '%i[new edit]'.
    Additional:
    - add Gemfile to create erd pdf.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :dialogs, except: %i[new end]
+  resources :dialogs, except: %i[new edit]
   # RESTful routes
   resources :examples, except: %i[new edit]
 
